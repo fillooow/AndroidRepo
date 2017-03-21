d0244d2733fd01fb422518f0eeb8570a68c34862
fillooow <fil201121@yandex.ru>
2017-03-21 21:47:02 +0500

    Study6.final

--- StarbuzzCoffee/app/src/main/java/com/hfad/starbuzzcoffee/DrinkActivity.java
+++ StarbuzzCoffee/app/src/main/java/com/hfad/starbuzzcoffee/DrinkActivity.java
@@ -3,6 +3,7 @@ package com.hfad.starbuzzcoffee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends AppCompatActivity {
     public static final String EXTRA_DRINKNO = "drinkNo";
@@ -17,8 +18,18 @@ public class DrinkActivity extends AppCompatActivity {
 
         ImageView photo = (ImageView) findViewById(R.id.photo);
         //источник данных графического поля назначается вызовом sIR()
+        //либо в макете android:src
         photo.setImageResource(drink.getImageResourceId());
         //это для повышения уровня доступности приложения
+        //либо можно в макете прописать android:contentDescription
         photo.setContentDescription(drink.getName());
+
+        //название напитка
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        //описание напитка
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(drink.getDescription());
     }
 }
