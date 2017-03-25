ad267878e5218ca656618dd85a722fcd15bd0b8d
fillooow <fil201121@yandex.ru>
2017-03-25 20:40:35 +0500

    PasswordDB1.1

--- YourOwnPasswordDatabase/app/src/main/java/com/filloownovac/yourownpassworddatabase/MainActivity.java
+++ YourOwnPasswordDatabase/app/src/main/java/com/filloownovac/yourownpassworddatabase/MainActivity.java
@@ -2,12 +2,34 @@ package com.filloownovac.yourownpassworddatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText passwordField = (EditText) findViewById(R.id.passField);
+    String passwordDB = passwordField.getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onOpen(View view) {
+        FileWorker.openDB(pathDB,passwordDB);
+    }
+
+    public void onCheckBoxClicked(View view){
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.checkbox_backup:
+                if (checked)
+                    FileWorker.isBackupNeed = true;
+                else
+                    FileWorker.isBackupNeed = false;
+                break;
+        }
+    }
+
 }
