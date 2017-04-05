be5d73ffc5de6b1c61de1b8ea8b418ac4ac25117
fillooow <fil201121@yandex.ru>
2017-04-06 00:40:56 +0500

    Clipboard1.2
    пилю сохранение

--- ClipboardProject/app/src/main/java/com/fillooow/clipboardproject/MainActivity.java
+++ ClipboardProject/app/src/main/java/com/fillooow/clipboardproject/MainActivity.java
@@ -3,21 +3,35 @@ package com.fillooow.clipboardproject;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String APP_PREFERENCES = "mysettings"; // Имя файла настроек
+    private static final String PREF_TEXT = "ClipboardText"; // Текст из буфера
     String checkPasteData = "";
     String pasteData = "";
+    SharedPreferences mSettings; // Переменная, представляющая экземпляр класса
+    // мы инициализируем её в onCreate()
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        refreshDat();
+        // Передаём в указанный метод название нашего файла (он будет создан
+        // автоматически) и разрешение, дающее доступ только
+        // компонентам приложения
+        if(mSettings.contains(APP_PREFERENCES)){
+            TextView tw = (TextView) findViewById(R.id.clipboard_text);
+            tw.append(mSettings.getString(APP_PREFERENCES, ""));
+        }
+        mSettings = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
+        //refreshDat();
     }
 
     public void refreshDat () {
@@ -29,10 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
                 pasteData = (String) item.getText();
-                if (checkPasteData.equals(pasteData)) {
-
-                }
-                else {
+                if (!(checkPasteData.equals(pasteData))) {
                     textView.append("\n" + pasteData);
                     checkPasteData = pasteData;
                 }
@@ -51,5 +62,16 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.clipboard_text);
         textView.append("\n" + pasteData);
         checkPasteData = pasteData;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        TextView textView = (TextView) findViewById(R.id.clipboard_text);
+        String textToSave = textView.getText().toString(); // Вытаскиваем текст
+        // для его сохранения
+        SharedPreferences.Editor editor = mSettings.edit(); // Получаем ссылку на эдитор
+        editor.putString(PREF_TEXT, textToSave); // Сейвим текст
+        editor.apply();
     }
 }
