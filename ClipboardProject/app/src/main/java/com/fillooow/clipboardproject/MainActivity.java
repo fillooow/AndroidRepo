fa89c95ed16d2c2b25db75223894a959866b6144
fillooow <fil201121@yandex.ru>
2017-04-06 22:23:38 +0500

    Clipboard1.3

--- ClipboardProject/app/src/main/java/com/fillooow/clipboardproject/MainActivity.java
+++ ClipboardProject/app/src/main/java/com/fillooow/clipboardproject/MainActivity.java
@@ -8,16 +8,19 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private StringBuffer sharedPreferencesText = new StringBuffer("");
+    private String text = "";
+    private String textOnSave = "";
+    String pasteData = "";
+    String checkPasteData = "";
     private static final String APP_PREFERENCES = "mysettings"; // Имя файла настроек
     private static final String PREF_TEXT = "ClipboardText"; // Текст из буфера
-    String checkPasteData = "";
-    String pasteData = "";
     SharedPreferences mSettings; // Переменная, представляющая экземпляр класса
     // мы инициализируем её в onCreate()
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +29,22 @@ public class MainActivity extends AppCompatActivity {
         // Передаём в указанный метод название нашего файла (он будет создан
         // автоматически) и разрешение, дающее доступ только
         // компонентам приложения
-        if(mSettings.contains(APP_PREFERENCES)){
-            TextView tw = (TextView) findViewById(R.id.clipboard_text);
-            tw.append(mSettings.getString(APP_PREFERENCES, ""));
-        }
+        textView = (TextView) findViewById(R.id.clipboard_text);
         mSettings = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
-        //refreshDat();
+
+        //if(mSettings.contains(APP_PREFERENCES)){
+        //textView.setText(mSettings.getString(APP_PREFERENCES, "123"));
+       // }
+        refreshDat();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mSettings.contains(APP_PREFERENCES)) {
+            savedText = mSettings.getString(PREF_TEXT, "123");
+            textView.setText(savedText);
+        }
     }
 
     public void refreshDat () {
@@ -40,14 +53,16 @@ public class MainActivity extends AppCompatActivity {
         handler.post(new Runnable() { //post() обеспечивает мгновенное выполнение кода
             @Override
             public void run() {
+                // Получаем ссылку на буфер обмена
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
-                pasteData = (String) item.getText();
-                if (!(checkPasteData.equals(pasteData))) {
-                    textView.append("\n" + pasteData);
-                    checkPasteData = pasteData;
+                ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0); // Получаем первый айтем из буфера обмена
+                text = (String) item.getText();
+                if (!(checkPasteData.equals(text))) {
+                    textView.append("\n" + text);
+                    checkPasteData = text;
+                    sharedPreferencesText = sharedPreferencesText.append(text + "\n");
                 }
-                handler.postDelayed(this, 100);
+                handler.post(this);
             }
         });
     }
@@ -55,19 +70,34 @@ public class MainActivity extends AppCompatActivity {
     public void onClickRefresh(View view) {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
         ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
-        pasteData = (String) item.getText();
-        if (checkPasteData.equals(pasteData))
+        text = (String) item.getText();
+        if (checkPasteData.equals(text))
             return;
 
         TextView textView = (TextView) findViewById(R.id.clipboard_text);
-        textView.append("\n" + pasteData);
-        checkPasteData = pasteData;
+        textView.append("\n" + text);
+        checkPasteData = text;
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        TextView textView = (TextView) findViewById(R.id.clipboard_text);
+    protected void onSaveInstanceState(Bundle outState) {
+        //textOnSave = textView.getText().toString();
+        textOnSave = sharedPreferencesText.toString();
+        outState.putString("savedText", textOnSave);
+        sharedPreferencesText = textOnSave.to`
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        textOnSave = savedInstanceState.getString("savedText");
+        textView.setText(textOnSave);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
         String textToSave = textView.getText().toString(); // Вытаскиваем текст
         // для его сохранения
         SharedPreferences.Editor editor = mSettings.edit(); // Получаем ссылку на эдитор
