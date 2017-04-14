5fd26a5a6d1baa0e607fc2b1ea6dbf2a138e1e11
fillooow <fil201121@yandex.ru>
2017-04-14 20:31:52 +0500

    Study12

--- StarbuzzCoffee/app/src/main/java/com/hfad/starbuzzcoffee/DrinkActivity.java
+++ StarbuzzCoffee/app/src/main/java/com/hfad/starbuzzcoffee/DrinkActivity.java
@@ -1,5 +1,8 @@
 package com.hfad.starbuzzcoffee;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -14,6 +17,28 @@ public class DrinkActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drink);
 
         int drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
+
+        try {
+            SQLiteOpenHelper starbuzzDatabaseHelper = new SturbuzzDatabaseHelper(this);
+            // Открываем БД для чтения
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            // Создаём курсор
+            Cursor cursor = db.query("DRINK",
+                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    "_id = ?", // Получаем запись через id
+                    new String[] {Integer.toString(drinkNo)},
+                    null, null, null);
+            // Переходим к первой записи в курсоре
+            if (cursor.moveToFirst()) {
+                // Даже если запись одна, всё равно получаем её
+                // Получаем данные из напитка
+                String nameText = cursor.getString(0); // Получаем NAME
+                String getDescription = cursor.getString(1); // Получаем DESCRIPTION
+                int photoId = cursor.getInt(2);                // Название напитка
+                TextView name = (TextView) findViewById(R.id.name);
+                name.setText(nameText);
+            }
+        }
         Drink drink = Drink.drinks[drinkNo];
 
         ImageView photo = (ImageView) findViewById(R.id.photo);
@@ -24,9 +49,7 @@ public class DrinkActivity extends AppCompatActivity {
         // Либо можно в макете прописать android:contentDescription
         photo.setContentDescription(drink.getName());
 
-        // Название напитка
-        TextView name = (TextView) findViewById(R.id.name);
-        name.setText(drink.getName());
+
 
         // Описание напитка
         TextView description = (TextView) findViewById(R.id.description);
